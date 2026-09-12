b856fa404324271de57ae3967cb00a586716f1d0
david hanley <ctchrinthry@gmail.com>
2026-09-12 13:41:33 -0700

    tokenizer moved to FLAG

--- internal/compiler/parser.go
+++ internal/compiler/parser.go
@@ -1,34 +1,17 @@
 package compiler
 
+import flagrt "flag-lang/runtime"
+
 func ParseFile(source string) (FileAST, error) {
-	return ParseTokenChannel(TokenizeSourceToChannel(source))
+	return fileASTFromFLAGTokens(flagrt.Call(compiler__tokenize_source, flagrt.NewString(source)))
 }
 
 // ParseSourceToChannel tokenizes source and streams top-level AST forms.
 func ParseSourceToChannel(source string) <-chan ASTForm {
-	return BuildASTFromTokens(TokenizeSourceToChannel(source))
+	return drainFLAGAST(flagrt.Call(compiler__build_ast_from_tokens, flagrt.Call(compiler__tokenize_source, flagrt.NewString(source))))
 }
 
 // ParseFileToChannel tokenizes a source file and streams top-level AST forms.
 func ParseFileToChannel(path string) <-chan ASTForm {
-	return BuildASTFromTokens(TokenizeFileToChannel(path))
-}
-
-func isSignedDecimalInteger(token string) bool {
-	if token == "" {
-		return false
-	}
-	start := 0
-	if token[0] == '+' || token[0] == '-' {
-		if len(token) == 1 {
-			return false
-		}
-		start = 1
-	}
-	for i := start; i < len(token); i++ {
-		if token[i] < '0' || token[i] > '9' {
-			return false
-		}
-	}
-	return true
+	return drainFLAGAST(flagrt.Call(compiler__build_ast_from_tokens, flagrt.Call(compiler__tokenize_file, flagrt.NewString(path))))
 }
